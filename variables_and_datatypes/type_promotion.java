6265648b9a1187438591065d00447430b76f450a
Diya Punetha <diyapunetha333@gmail.com>
2026-03-17 22:19:48 +0530

    Added loops practice

--- variables_and_datatypes/type_promotion.java
+++ variables_and_datatypes/type_promotion.java
@@ -14,12 +14,16 @@ public class type_promotion {
         byte bt = (byte) (a + b + c);
         System.out.println(bt); */
 
-        int a=10;
+        /* int a=10;
         float b=20.25f;
         long c =25;
         double d=30;
         double ans = a+b+c+d;
-        System.out.println(ans);
+        System.out.println(ans); */
+
+        byte b=5;
+        byte a = (byte) (b * 2);
+        System.out.println(a);
 
     }
 }
