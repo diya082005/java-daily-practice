dfb5d40bd54f40ce7fde1b26c2989a3ea3777dff
Diya Punetha <diyapunetha333@gmail.com>
2026-03-17 19:47:38 +0530

    Added loops practice

--- variables_and_datatypes/input_in_java.java
+++ variables_and_datatypes/input_in_java.java
@@ -3,5 +3,6 @@ import java.util.*;
 public class input_in_java {
     public static void main(String[] args) {
         Scanner sc= new Scanner (System.in);
+        String input =  sc.next();
     }
 }
