d3058bd6ecc048eab99dacab4731d5ec84e3d418
Diya Punetha <diyapunetha333@gmail.com>
2026-03-17 22:00:44 +0530

    Added loops practice

--- variables_and_datatypes/type_casting.java
+++ variables_and_datatypes/type_casting.java
@@ -1,9 +1,15 @@
-import java.util.*;
 public class type_casting {
     public static void main(String[] args) {
-        Scanner sc =  new Scanner(System.in);
+       /*  Scanner sc =  new Scanner(System.in);
         float a = 25.9999f;
         int b = (int)a;
-        System.out.println(b);
+        System.out.println(b); */
+
+        char ch ='a';
+        char ch2 = 'b';
+        int number = ch;
+        int number2 = ch2;
+        System.out.println(number);
+        System.out.println(number2);
     }
 }
