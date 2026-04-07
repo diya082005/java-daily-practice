d642ef9877b1093d6d8457b6aa01a66c703ed075
Diya Punetha <diyapunetha333@gmail.com>
2026-04-07 09:35:13 +0530

    Added Operators_java folder with Java files

---
+++ Operators_java/arithmetics.java
@@ -0,0 +1,11 @@
+public class arithmetics {
+
+    public static void main(String args[]){
+        int a=10;
+        int b=5;
+
+        System.out.println("add = " + (a+b));
+        System.out.println("sub = " + (a-b));
+    }
+    
+}
