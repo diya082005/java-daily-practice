45f932e9edd9107a624bc76321771554bee10a21
Diya Punetha <diyapunetha333@gmail.com>
2026-04-07 10:15:00 +0530

    update

--- Operators_java/arithmetic_unary.java
+++ Operators_java/arithmetic_unary.java
@@ -1,7 +1,12 @@
 public class arithmetic_unary {
     public static void main(String args[]){
-        int a=10;
+        /*int a=10;
         int b= ++a;
+        System.out.println(a);
+        System.out.println(b);*/
+
+        int a=10;
+        int b=a--;
         System.out.println(a);
         System.out.println(b);
     }
