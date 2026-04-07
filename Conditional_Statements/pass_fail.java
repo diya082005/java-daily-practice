9fb071cc826c23b9cbc33b4e80f4487dbff10ad4
Diya Punetha <diyapunetha333@gmail.com>
2026-04-07 12:52:08 +0530

    update

---
+++ Conditional_Statements/pass_fail.java
@@ -0,0 +1,10 @@
+
+public class pass_fail {
+    public static void main(String args[]){
+        int marks = 67;
+
+        String reportCard = marks>=33 ? "pass":"fail";
+        System.out.println(reportCard);
+    }
+    
+}
---
+++ Conditional_Statements/statements.java
@@ -0,0 +1,44 @@
+public class statements {
+    public static void main(String args[]){
+        //int age = 22;
+
+        /* if-else
+        if(age>18){
+            System.out.println("adult : drive, vote");
+        }
+        else{
+            System.out.println("not adult");
+        } 
+        */     
+        /*
+        if(age>13 && age<18){
+            System.out.println("teenager");
+        }else{
+            System.out.println("not adult");
+        }
+        */
+       /* else-if
+       if (age >=18){
+        System.out.println("adult");
+       }
+       else if(age>=13 && age<18){
+        System.out.println("teenager");
+       }
+       else{
+        System.out.println("child");
+       }
+       */
+        int number=2;
+        switch(number){
+            case 1: System.out.println("samosa");
+                    break;
+            case 2: System.out.println("burger");
+                    break;
+            case 3: System.out.println("chaat");
+                    break;
+            default: System.out.println("We are dreaming");
+        }
+
+    }
+    
+}
