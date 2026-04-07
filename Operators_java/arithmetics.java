f9120c2e1b86aa3a2280e2881f59328d46707089
Diya Punetha <diyapunetha333@gmail.com>
2026-04-07 09:41:51 +0530

    update

--- Operators_java/arithmetics.java
+++ Operators_java/arithmetics.java
@@ -6,6 +6,9 @@ public class arithmetics {
 
         System.out.println("add = " + (a+b));
         System.out.println("sub = " + (a-b));
+        System.out.println("mul = " + (a*b));
+        System.out.println("div = " + (a/b));
+        System.out.println("modulo(remainder) = " + (a%b));
     }
     
 }
