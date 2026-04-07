4f233c658620d3fba358aea8ff675d8ee6c58b66
Diya Punetha <diyapunetha333@gmail.com>
2026-04-07 10:06:39 +0530

    update

---
+++ Operators_java/arithmetic_binary.java
@@ -0,0 +1,15 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class arithmetic_binary {
+   public arithmetic_binary() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 10;
+      byte var2 = 5;
+      System.out.println("add = " + (var1 + var2));
+      System.out.println("sub = " + (var1 - var2));
+      System.out.println("mul = " + var1 * var2);
+      System.out.println("div = " + var1 / var2);
+      System.out.println("modulo(remainder) = " + var1 % var2);
+   }
+}
