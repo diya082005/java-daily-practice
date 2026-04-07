a29396a6a78c6becc00d88f10e01e2c028241ba0
Diya Punetha <diyapunetha333@gmail.com>
2026-04-07 10:28:10 +0530

    update

---
+++ Operators_java/Assignment_Op.java
@@ -0,0 +1,14 @@
+public class Assignment_Op {
+    public static void main(String args[]){
+        int a =10;
+        //a=a+10;
+        a+=10;
+
+        int b=5;
+        b-=5;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+    
+}
