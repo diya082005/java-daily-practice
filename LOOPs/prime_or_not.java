0b105724f493255d31604bbb789097e6bd185547
Diya Punetha <diyapunetha333@gmail.com>
2026-04-07 16:05:25 +0530

    update

--- LOOPs/prime_or_not.java
+++ LOOPs/prime_or_not.java
@@ -9,7 +9,7 @@ public class prime_or_not {
             System.out.println("n is prime");
         }else{
             boolean isPrime = true;
-            for(int i=2 ; i<=n-1 ; i++){
+            for(int i=2 ; i<=Math.sqrt(n) ; i++){
                 if(n % i == 0){ //n is a multiple of i ( i is not equal to 1 or n)
                     isPrime = false;
                 }
